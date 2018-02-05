a6b24f2a55a6c9c92c8efe534aba7e8b199311e0
Anil Patel <AnilJayPatel@Anils-MacBook-Pro-2.local>
2018-02-05 11:23:29 -0600

    logic is done, need to style and print wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,10 @@ var numWhite = 0;
 var numBlue = 0;
 var numPink = 0;
 var totalNumber = 0;
+var numRedRando = Math.floor(Math.random() * 25);
+var numWhiteRando = Math.floor(Math.random() * 25);
+var numBlueRando = Math.floor(Math.random() * 25);
+var numPinkRando = Math.floor(Math.random() * 25);
 
 
 
@@ -14,61 +18,88 @@ var totalNumber = 0;
         
         totalNumber = 0;
         $("#totalNumber").text(totalNumber);
+        
 
         numRed = 0;
         $("#redNumber").text(numRed);
+        numRedRando = Math.floor(Math.random() * 25);
 
         numWhite = 0;
         $("#whiteNumber").text(numWhite);
+        numWhiteRando = Math.floor(Math.random() * 25);
 
         numBlue = 0;
         $("#blueNumber").text(numBlue);
+        numBlueRando = Math.floor(Math.random() * 25);
 
         numPink = 0;
         $("#pinkNumber").text(numPink);
+        numPinkRando = Math.floor(Math.random() * 25);
+
+
 
    });
 
     $("#redGem").on("click", function () {
-        var numRedRando = Math.floor(Math.random() * 25);
-        numRed += numRedRando; 
+        numRed = numRedRando; 
         $("#redNumber").text(numRed);
         totalNumber += numRed;
         
         $("#totalNumber").text(totalNumber);
+
+        // logic for the game:
+        if (totalNumber > numTarget) {
+            console.log("loss");
+        } else if (totalNumber == numTarget) {
+            console.log("win");
+        }
     });
 
     $("#whiteGem").on("click", function () {
-        var numWhiteRando = Math.floor(Math.random() * 25);
-        numWhite += numWhiteRando;
+        numWhite = numWhiteRando;
         $("#whiteNumber").text(numWhite);
         totalNumber += numWhite;
 
         $("#totalNumber").text(totalNumber);
+
+        // logic for the game:
+        if (totalNumber > numTarget) {
+            console.log("loss");
+        } else if (totalNumber == numTarget) {
+            console.log("win");
+        }
     });
 
     $("#blueGem").on("click", function () {
-        var numBlueRando = Math.floor(Math.random() * 25);
-        numBlue += numBlueRando;
+        numBlue = numBlueRando;
         $("#blueNumber").text(numBlue);
         totalNumber += numBlue;
 
         $("#totalNumber").text(totalNumber);
+
+        // logic for the game:
+        if (totalNumber > numTarget) {
+            console.log("loss");
+        } else if (totalNumber == numTarget) {
+            console.log("win");
+        }
     });
 
     $("#pinkGem").on("click", function () {
-        var numPinkRando = Math.floor(Math.random() * 25);
-        numPink += numPinkRando;
+        numPink = numPinkRando;
         $("#pinkNumber").text(numPink);
         totalNumber += numPink;
 
         $("#totalNumber").text(totalNumber);
+
+        // logic for the game:
+        if (totalNumber > numTarget) {
+            console.log("loss");
+        } else if (totalNumber == numTarget) {
+            console.log("win");
+        }
     });
 
 
-    // logic for the game:
-       if (totalNumber > numTarget) {
-           console.log("loss");
-       } else if (totalNumber == numTarget) {
-           console.log("win");
-       }
+
+// use the fridge principles to solidify the values and stay random for that turn, and then look up the document. ready property
